9f192dbf48f9f282e3421bca4238e9661587538b
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2023-06-14 11:34:31 +0800

    lab4: Updated correct Collatz implementation

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,12 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Returns the next number for Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
