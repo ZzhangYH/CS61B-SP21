6e420f05db10627af69b97cb037cc9bf28b20b6c
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2023-06-28 23:10:14 +0800

    lab7: Refactor `BSTMap` comments

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -33,33 +33,36 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
+    /** Root node of the BSTMap. */
     private Node root;
 
+    /** Size of the BSTMap. */
     private int size;
 
+    /** Constructor for an empty BSTMap. */
     public BSTMap() {
         clear();
     }
 
-    /* Removes all of the mappings from this map. */
+    /** Removes all of the mappings from this map. */
     public void clear() {
         root = null;
         size = 0;
     }
 
-    /* Returns the number of key-value mappings in this map. */
+    /** Returns the number of key-value mappings in this map. */
     public int size() {
         return size;
     }
 
-    /* Returns true if this map contains a mapping for the specified key. */
+    /** Returns true if this map contains a mapping for the specified key. */
     public boolean containsKey(K key) {
         Node T = find(root, key);
         return T != null;
     }
 
-    /* Returns the value to which the specified key is mapped, or null if this
-     * map contains no mapping for the key. */
+    /** Returns the value to which the specified key is mapped, or null if this
+     *  map contains no mapping for the key. */
     public V get(K key) {
         Node T = find(root, key);
         if (T == null) {
@@ -69,12 +72,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Associates the specified value with the specified key in this map. */
+    /** Associates the specified value with the specified key in this map. */
     public void put(K key, V val) {
         root = put(root, key, val);
         size += 1;
     }
 
+    /** Helper method for put, recursively search and compare, then put the key. */
     private Node put(Node T, K key, V val) {
         if (T == null) {
             return new Node(key, val);
@@ -90,9 +94,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return T;
     }
 
-    /* Removes the mapping for the specified key from this map if present.
-     * Not required for Lab 7. If you don't implement this, throw an
-     * UnsupportedOperationException. */
+    /** Removes the mapping for the specified key from this map if present. */
     public V remove(K key) {
         // Removing the root node.
         if (root.key == key) {
@@ -127,9 +129,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Removes the entry for the specified key only if it is currently mapped to
-     * the specified value. Not required for Lab 7. If you don't implement this,
-     * throw an UnsupportedOperationException.*/
+    /** Removes the entry for the specified key only if it is currently mapped to
+     *  the specified value. */
     public V remove(K key, V value) {
         if (get(key) == value) {
             return remove(key);
@@ -138,7 +139,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Hibbard deletion case for the remove method. */
+    /** Removes node with two children (Hibbard deletion), substitute must be
+     *  < than everything in the left subtree and
+     *  > than everything in the right subtree. */
     private Node hibbardRemove(Node T) {
         ArrayList<K> keys = keysInOrder(T);
         int idx = keys.indexOf(T.key) + 1;
@@ -157,8 +160,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return newT;
     }
 
-    /* Searches for the specified key underneath the specified node, and
-     * returns the Node with the key or null if not found. */
+    /** Returns the node with the specified key, or null if key not found. */
     private Node find(Node T, K key) {
         if (T == null) {
             return null;
@@ -173,8 +175,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Driver method for finding the parent node of the specified key, starting from
-     * the root node. */
+    /** Returns the parent node of the node with the specified key, null if key not found,
+     *  or root if the key is found at root. */
     private Node findParent(K key) {
         if (root == null) {
             return null;
@@ -189,7 +191,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Helper method for findParent, keeping track of the parent. */
+    /** Helper method for findParent, keeping track of the parent during recursion. */
     private Node findParent(Node P, Node T, K key) {
         if (T == null) {
             return null;
@@ -204,7 +206,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Prints out BSTMap in order of increasing key. */
+    /** Prints out BSTMap in order of increasing key. */
     public void printInOrder() {
         if (root == null) {
             System.out.println("There is nothing in the BSTMap.");
@@ -216,7 +218,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    /* Returns an ArrayList of keys in increasing order. */
+    /** Returns an ArrayList of keys in ascending order. */
     private ArrayList<K> keysInOrder(Node T) {
         ArrayList<K> keys = new ArrayList<>();
         if (T.left != null) {
@@ -229,13 +231,12 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return keys;
     }
 
-    /* Returns a Set view of the keys contained in this map. Not required for Lab 7.
-     * If you don't implement this, throw an UnsupportedOperationException. */
+    /** Returns a Set view of the keys contained in this map. */
     public Set<K> keySet() {
         return new HashSet<>(keysInOrder(root));
     }
 
-    /* Return the Iterator for the BSTMap utilizing the set Iterator. */
+    /** Returns BSTMap Iterator utilizing Set Iterator. */
     public Iterator<K> iterator() {
         return keySet().iterator();
     }
